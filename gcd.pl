b41529674834655c7b0be9345807dc02b1fbbf61
Anshuman Giramkar <99130545+AnshumanGiramkar@users.noreply.github.com>
2024-05-28 22:44:02 +0530

    Update gcd.pl

--- gcd.pl
+++ gcd.pl
@@ -1,4 +1,7 @@
-gcd(A,B,X):- A=0,X=B. % base case
-gcd(A,B,X):- B=0,X=A. % base case
-gcd(A,B,X):- A>B, gcd(B, A, X).
-gcd(A,B,X):- A<B, T is B mod A, gcd(A, T, X).
+% Base cases
+gcd(A, 0, A) :- A > 0.
+gcd(0, B, B) :- B > 0.
+
+% Recursive cases
+gcd(A, B, X) :- A > B, gcd(B, A, X).
+gcd(A, B, X) :- A =< B, T is B mod A, gcd(A, T, X).
